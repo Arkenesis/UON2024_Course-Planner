2bbc8161c7843438ced30ee950a9d2e46ee44333
NiceFinal1230 <lohdiingyang@gmail.com>
2024-05-14 14:07:38 +0800

    can work 1

--- New_Frontend/course_planner/src/App.jsx
+++ New_Frontend/course_planner/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from './react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <PlanYourPath>
+      <div>hi</div>
+      <div>Count: {count}</div>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
