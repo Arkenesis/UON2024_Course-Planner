350554216877961150fceb3cefbf040684e59100
NiceFinal1230 <lohdiingyang@gmail.com>
2024-05-28 18:48:21 +0800

    Kenndy work

--- New_Frontend/course_planner/src/App.jsx
+++ New_Frontend/course_planner/src/App.jsx
@@ -13,10 +13,26 @@ import TermsAndConditions from "./pages/termsandconditions/TermsAndConditions.js
 import PrivacyPolicy from "./pages/privacypolicy/PrivacyPolicy.jsx";
 import Footer from "./components/Footer.jsx";
 
+import NavigationBar from './components/NavigationBar';
+import Sidebar from './components/Sidebar';
+import MainContent from './components/MainContent';
+
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
 
   return (
     <>
+      <div className="App">
+        <NavigationBar />
+        <div className="container">
+          <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+          <MainContent />
+        </div>
+      </div>
       <AboutUs />
       <Contact />
       <Register/>
